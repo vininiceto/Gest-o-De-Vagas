46aaf8f178cabde15e09c7936a96ca99d76136d3
vininiceto <viniciusniceto@gmail.com>
2025-11-30 11:57:06 -0300

    Adicionando as entidades de vagas e companhias.

--- src/main/java/br/com/vininiceto/modules/company/entities/CompanyEntity.java
+++ src/main/java/br/com/vininiceto/modules/company/entities/CompanyEntity.java
@@ -1,4 +1,4 @@
-package br.com.vininiceto.modules.candidate.entities;
+package br.com.vininiceto.modules.company.entities;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -7,33 +7,40 @@ import jakarta.persistence.Id;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.validator.constraints.Length;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
+@Entity(name = "company")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@EqualsAndHashCode
-@Entity(name = "candidate")
-public class CandidateEntity {
+public class CompanyEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
-    private String name;
-    @NotBlank
     @Pattern(regexp = "^[^\\s]+$", message = "O campo [username] não deve conter espaços")
+    private String nome;
+    @NotBlank
+    @Length(max = 14)
+    private String cnpj;
+    @NotBlank
     private String username;
     @Email(message = "O campo(email) deve conter um e-mail válido!")
-    @NotBlank
     private String email;
-    @Length(min = 10, max = 25)
-    @NotBlank
+    @Length(min = 10, max = 25, message = "A senha deve conter entre (10) e (25) caracteres")
     private String password;
+    private String website;
     private String description;
-    private String curriculum;
+    @CreationTimestamp
+    private LocalDateTime createdAt;
 
 
 }
